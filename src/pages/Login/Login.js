f0809e0e3afa00463f88addf7af0d5c1e5c92ffb
Arthur Martins Braga <arthurmbraga1@gmail.com>
2020-10-06 23:10:47 -0300

    Login funcional

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const history = useHistory();
+
+  function login() {
+    if (email === "arthur@cpejr.com.br" && password === "123") {
+      alert("Bem vindo!\n" + email);
+      history.push("home");
+    } else alert("Dados incorretos");
+  }
+
   return (
     <div className="base">
-      <div className="container">
-        <div>
-          <img src="/images/Quick_note.png" alt="Quick note" />
-          <Form className="inputs">
-            <Form.Group controlId="email">
-              <Form.Control type="email" placeholder="Email" />
-            </Form.Group>
-            <Form.Group controlId="senha">
-              <Form.Control type="password" placeholder="Senha" />
-            </Form.Group>
-            <Button variant="primary">Login</Button>
-          </Form>
-        </div>
+      <div className="container1">
+        <img className="image" src="/images/Quick_note.png" alt="Quick note" />
+        <Form className="inputs">
+          <Form.Group controlId="email">
+            <Form.Control
+              type="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="senha">
+            <Form.Control
+              type="password"
+              placeholder="Senha"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" onClick={login}>
+            Login
+          </Button>
+        </Form>
       </div>
     </div>
   );
