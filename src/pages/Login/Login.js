7ef67254e79ba09e5230d1e589e0e0684eb5904a
Arthur Martins Braga <arthurmbraga1@gmail.com>
2020-10-06 22:53:19 -0300

    HTML CSS

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
+import "./Login.css";
 
 function Login() {
-  return <h1>Login</h1>;
+  return (
+    <div className="base">
+      <div className="container">
+        <div>
+          <img src="/images/Quick_note.png" alt="Quick note" />
+          <Form className="inputs">
+            <Form.Group controlId="email">
+              <Form.Control type="email" placeholder="Email" />
+            </Form.Group>
+            <Form.Group controlId="senha">
+              <Form.Control type="password" placeholder="Senha" />
+            </Form.Group>
+            <Button variant="primary">Login</Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
